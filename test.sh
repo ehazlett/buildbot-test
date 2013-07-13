33c9981ba8b6db0cd72b0ed2aa28a672778f01d4
Evan Hazlett <ejhazlett@gmail.com>
2013-07-13 19:24:31 -0400

    bump

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "Building VE..."
 virtualenv --no-site-packages ve
+echo "Installing requirements..."
 ./ve/bin/pip install -r requirements.txt
 ./ve/bin/python manage.py test demo
 rm -rf ./ve
